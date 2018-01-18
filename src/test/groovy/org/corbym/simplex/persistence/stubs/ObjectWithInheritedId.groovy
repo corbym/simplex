01d145d1bddd001530f627e4f5d6def5fb621007
Matt <matt.corby@gmail.com>
2018-01-18 22:13:30 +0000

    Update ObjectWithInheritedId.groovy

--- src/test/groovy/org/corbym/simplex/persistence/stubs/ObjectWithInheritedId.groovy
+++ src/test/groovy/org/corbym/simplex/persistence/stubs/ObjectWithInheritedId.groovy
@@ -1,12 +1,5 @@
 package org.corbym.simplex.persistence.stubs
 
-/**
- * Created by IntelliJ IDEA.
- * User: Matt.Corby
- * Date: 20/02/11
- * Time: 19:49
- * To change this template use File | Settings | File Templates.
- */
 class ObjectWithInheritedId extends SomeObjectWithId {
     def someotherfield
 }
